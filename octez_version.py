7fcdfdc14bc5a62431e494180b670650911fa8d4
Lev Gorodetskiy <github@droserasprout.space>
2024-02-04 16:57:14 -0300

    login to git first

--- octez_version.py
+++ octez_version.py
@@ -12,7 +12,6 @@ OCTEZ_REPO_URL = 'https://github.com/serokell/tezos-packaging/releases/latest'
 
 
 def main():
-
     version_file = Path('octez_version')
     current_version = version_file.read_text().strip() if version_file.exists() else ''
     print(f'Current version: {current_version}')
@@ -20,22 +19,18 @@ def main():
     print(f'Latest version:  {latest_version}')
 
     if current_version == latest_version:
-        return
+        print('Octez version is up to date.')
+        exit(1)
 
     if not env.get("GITHUB_ACTIONS"):
         print("This script is intended to be run in a GitHub Action.")
-        exit(1)
+        exit(2)
 
     print('Octez version has changed. Updating...')
     Path('octez_version').write_text(latest_version)
 
     run('make', 'build')
-    run('git', 'checkout', '-b', f'octez-{latest_version}')
-    run('git', 'add', 'octez_version')
-    run('git', 'commit', '-m', f'Update Octez binaries to {latest_version}')
-    run('git', 'push', 'origin', f'octez-{latest_version}')
-    run('gh', 'pr', 'create', '-f', '-B', f'octez-{latest_version}', '-t', f'Update Octez binaries to {latest_version}')
-
+    exit(0)
 
 if __name__ == "__main__":
     main()
